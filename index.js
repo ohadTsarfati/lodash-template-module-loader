5cb09ec68b5a12c682a87d7d22744bb5e4e1cf2f
ohad tsarfati <ohad.tsarfati@gmail.com>
2019-02-15 00:09:27 +0200

    some bug fixes

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
+const path = require('path');
+
 const _ = require('lodash');
 const lodaerUtils = require('loader-utils');
 
 
 const defaultOptions = {
-  //interpolate: _.templateSettings.interpolate,
-  //escape: _.templateSettings.escape,
-  //evaluate: _.templateSettings.evaluate,
-  //variable: _.templateSettings.variable,
-  perpendFileNameComment: false,
+  // interpolate: _.templateSettings.interpolate,
+  // escape: _.templateSettings.escape,
+  // evaluate: _.templateSettings.evaluate,
+  // variable: _.templateSettings.variable,
+  perpendFileNameComment: '',
   asESmodule: false,
   importsModulePath: '',
-  enginge: 'lodash',
+  engine: 'lodash',
   globalEngine: false,
   disableImports: false,
 };
 
 
 module.exports = function(source) {
-  const template = source;
-  let compiledTemplateModule = '';
+  let template = source;
+  let compiledTemplate = '';
   // Caching
   this.cacheable && this.cacheable();
 
@@ -27,58 +29,73 @@ module.exports = function(source) {
 
   // Aggrgating the options
   const options = _.defaults(
-    {},
-    userPref,
-    defaultOptions
+      {},
+      userPref,
+      defaultOptions
   );
 
-  // Aggregating template settings: interpolate, escape, avaluate, variable. Fallsback to default _.templateSettings object
-  _.templateSettings = _.defaults({},
-    _.chain(options)
-      .pick(['interpolate', 'escape', 'evaluate'])
-      .reduce((reduction, val, key) => {
+  // aggregate template settings: interpolate, escape, avaluate, variable.
+  // fallsback to default _.templateSettings object
+  _.templateSettings = _.defaults({}, _.chain(options).
+      pick(['interpolate', 'escape', 'evaluate']).
+      reduce((reduction, val, key) => {
         reduction[key] = new RegExp(val, 'g');
         return reduction;
-      }, {})
-      .value(),
-    _.pick(options, 'variable'),
-    _.templateSettings
+      }, {}).
+      value(),
+  _.pick(options, 'variable'),
+  _.templateSettings
   );
 
-  const { prependFilenameComment, asESmodule } = options;
-  if (prependFilenameComment) {
-    template = `<!-- ${path.relative(prependFilenameComment, this.resource)} -->\n${template}`;
+  const {
+    prependFileNameComment,
+    asESmodule,
+    globalEngine,
+    engine,
+  } = options;
+
+  // append file name comment
+  if (!_.isEmpty(prependFileNameComment) && _.isString(prependFileNameComment)) {
+    template = `<!-- ${path.relative(prependFileNameComment, this.resource)} -->\n ${template}`;
   }
 
   const templateSource = _.template(template).source;
 
   if (asESmodule) {
+    if (!globalEngine) {
+      compiledTemplate = `import _ from '${engine}';\n`;
+    }
     // simply exporting the compiled template as ES6 module
-    compiledTemplateModule = `export default ${templateSource};`;
-  } else {
-    const { importsModulePath, globalEngine, engine, disableImports } = options;
-    let imports = '';
-    // Checking if imports module has been provided
-    if (!_.isEmpty(importsModulePath)) {
-      imports = `require('${importsModulePath}')`;
-    } else if (engine === 'lodash') { // If no imports module provided and the engine is lodash the imports are the _.templateSettings.imports        
-      imports = `(typeof _ !== 'undefined') ? _.templateSettings.imports : {}`;
-    }
-    // If imports are provided and not explicitly disabling imports
-    if (!_.isEmpty(imports) && !disableImports) {
-      if (engine !== 'lodash' || engine !== 'underscore') {
-        throw new Error(`When using imports the enginge must be either 'lodash' or 'underscore'`);
-      }
-      if (!globalEngine) {
-        compiledTemplateModule = `var _ = require('${engine}');\n`;
-      }
-      compiledTemplateModule = `${compiledTemplateModule}var imports = ${imports}\n`;
-      compiledTemplateModule = `${compiledTemplateModule}module.exports = Function(_.keys(imports), 'return ' + ${templateSource}.toString()).apply(undefined, _.values(imports));`
-    } else {
-      // When not imports or explicitly disabling it creates simple module export of the compiled template
-      compiledTemplateModule = `${compiledTemplateModule}\nmodule.exports = ${templateSource};\n`
-    }
+    return `${compiledTemplate}export default ${templateSource};`;
   }
 
-  return compiledTemplateModule;
+  const {
+    importsModulePath,
+    disableImports,
+  } = options;
+
+  let imports = '';
+  // Checking if imports module has been provided
+  if (!_.isEmpty(importsModulePath)) {
+    imports = `require('${importsModulePath}')`;
+  } else if (engine === 'lodash') {
+    // If no imports module provided and the engine is lodash the imports are the _.templateSettings.imports
+    imports = `(typeof _ !== 'undefined') ? _.templateSettings.imports : {}`;
+  }
+
+  if (!globalEngine) {
+    compiledTemplate = `var _ = require('${engine}');\n`;
+  }
+  // If imports are provided and not explicitly disabling imports
+  if (!_.isEmpty(imports) && !disableImports) {
+    if (engine !== 'lodash' && engine !== 'underscore') {
+      throw new Error(`When using imports the engine must be either 'lodash' or 'underscore'`);
+    }
+    return `${compiledTemplate}var imports = ${imports};\n` +
+    `module.exports = Function(_.keys(imports), ` +
+    `'return ' + ${templateSource}.toString()).apply(undefined, _.values(imports));`;
+  } else {
+    // When not imports or explicitly disabling it creates simple module export of the compiled template
+    return `${compiledTemplate}module.exports = ${templateSource};\n`;
+  }
 };
